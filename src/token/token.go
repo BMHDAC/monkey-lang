a4bb75a230e7dddc1693df25e8e033fc554aec9b
BMHDAC <bdachai308@gmail.com>
2024-08-22 23:33:48 +0700

    Finish basic lexer

--- src/token/token.go
+++ src/token/token.go
@@ -26,6 +26,18 @@ type Token struct {
 	Literal string
 }
 
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookUpIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
+
 // Define our basic token types
 const (
 	ILLEGAL = "ILLEGAL"
