1eea97327a4112418379b241dacfac8b27341913
Thomas <bdachai308@gmail.com>
2024-08-28 17:06:43 +0700

    Adding String methods for Statements

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -1,6 +1,9 @@
 package ast
 
-import "monkey/src/token"
+import (
+	"bytes"
+	"monkey/src/token"
+)
 
 // A token node of each parsed input
 type Node interface {
@@ -22,6 +25,18 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
 
 type ReturnStatement struct {
 	Token       token.Token
@@ -30,6 +45,17 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+
+	return out.String()
+}
 
 // Node of an identifier
 type Identifier struct {
@@ -39,6 +65,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 type Expression interface {
 	Node
@@ -52,6 +79,13 @@ type ExpressionStatement struct {
 
 func (es *ExpressionStatement) statementNode()       {}
 func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+
+	return ""
+}
 
 type Program struct {
 	Statements []Statement
@@ -63,4 +97,14 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return p.Statements[0].TokenLiteral()
 	}
+}
+
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
 }
