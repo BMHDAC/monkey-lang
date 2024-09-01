e3c3dde9a61c0d591b3b4b77acba266c0f70eccf
Thomas <bdachai308@gmail.com>
2024-09-02 01:41:10 +0700

    CallExpression partial

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -723,3 +723,38 @@ func TestFunctionParamsParsing(t *testing.T) {
 		}
 	}
 }
+
+func TestCallExpression(t *testing.T) {
+	input := "add(1, 2 * 3, 4 + 5);"
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParserError(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("len(program.Statements) is not 1, got: %d\n", len(program.Statements))
+	}
+
+	stm, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not ExpressionStatement, got: %T", program.Statements[0])
+	}
+
+	exp, ok := stm.Expression.(*ast.CallExpression)
+	if !ok {
+		t.Fatalf("stm.Expression is not CallExpression, got %T", stm.Expression)
+	}
+
+	if !testIdentifier(t, exp.Function, "add") {
+		return
+	}
+
+	if len(exp.Arguments) != 3 {
+		t.Fatalf("exp.Arguments not 3, got: %d", len(exp.Arguments))
+	}
+
+	testLiteralExpression(t, exp.Arguments[0], 1)
+	testInfixExpression(t, exp.Arguments[1], 2, "*", 3)
+	testInfixExpression(t, exp.Arguments[2], 4, "*", 5)
+}
