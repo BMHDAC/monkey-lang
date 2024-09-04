12c26e067fadfa7e0f4e14b556bd99664eeb0440
Thomas <bdachai308@gmail.com>
2024-09-04 14:42:38 +0700

    Evaluator infix and prefix'

--- src/evaluator/evaluator_test.go
+++ src/evaluator/evaluator_test.go
@@ -16,6 +16,20 @@ func TestEvalIntegerExpression(t *testing.T) {
 		{"5", 5},
 		{"10", 10},
 		{"42069", 42069},
+		{"-5", -5},
+		{"-10", -10},
+		{"-42069", -42069},
+		{"5 + 5 + 5 + 5 - 10", 10},
+		{"2 * 2 * 2 * 2 * 2", 32},
+		{"-50 + 100 + -50", 0},
+		{"5 * 2 + 10", 20},
+		{"5 + 2 * 10", 25},
+		{"20 + 2 * -10", 0},
+		{"50 / 2 * 2 + 10", 60},
+		{"2 * (5 + 10)", 30},
+		{"3 * 3 * 3 + 10", 37},
+		{"3 * (3 * 3) + 10", 37},
+		{"(5 + 10 * 2 + 15 / 3) * 2 + -10", 50},
 	}
 
 	for _, tt := range tests {
@@ -44,4 +58,74 @@ func testIntegerObject(t *testing.T, evaluated object.Object, expected int64) bo
 	}
 
 	return true
+}
+
+func TestEvalBooleanExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{
+			"true", true,
+		},
+		{
+			"false", false,
+		},
+		{"1 < 2", true},
+		{"1 > 2", false},
+		{"1 < 1", false},
+		{"1 > 1", false},
+		{"1 == 1", true},
+		{"1 != 1", false},
+		{"1 == 2", false},
+		{"1 != 2", true},
+		{"true == true", true},
+		{"false == false", true},
+		{"true == false", false},
+		{"true != false", true},
+		{"false != true", true},
+		{"(1 < 2) == true", true},
+		{"(1 < 2) == false", false},
+		{"(1 > 2) == true", false},
+		{"(1 > 2) == false", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
+}
+
+func testBooleanObject(t *testing.T, evaluated object.Object, expected bool) bool {
+	result, ok := evaluated.(*object.Boolean)
+	if !ok {
+		t.Errorf("Object is not Boolean, got: %T", evaluated)
+		return false
+	}
+
+	if result.Value != expected {
+		t.Errorf("Object value wrong, expected: %t, got: %t", expected, result.Value)
+		return false
+	}
+
+	return true
+}
+
+func TestBangOperator(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected bool
+	}{
+		{"!true", false},
+		{"!false", true},
+		{"!!true", true},
+		{"!!false", false},
+		{"!5", false},
+		{"!!5", true},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testBooleanObject(t, evaluated, tt.expected)
+	}
 }
